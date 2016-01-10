99f85cfd6dda054339da4134f2196f88cc635324
Bideo Wego <bideowego@gmail.com>
2016-01-10 17:48:49 -0500

    Refactor timer loop and #stop test

--- lib/slack_pomodoro_timer/timer.rb
+++ lib/slack_pomodoro_timer/timer.rb
@@ -7,6 +7,7 @@ module SlackPomodoroTimer
     def initialize(options={})
       @pomodoros = options[:pomodoros]
       @interval = options[:interval]
+      @stop = false
     end
 
 
@@ -18,12 +19,10 @@ module SlackPomodoroTimer
     def start(&block)
       @total = @pomodoros
       begin
-        loop do
-          yield(@pomodoros)
-          sleep(@interval)
-          @pomodoros -= 1
-          break if stop?
-        end
+        yield(@pomodoros)
+        sleep(@interval)
+        @pomodoros -= 1
+        start(&block) unless stop?
       rescue SystemExit, Interrupt
         # rescues interrupt to allow
         # quit with ctrl-c
@@ -44,7 +43,7 @@ module SlackPomodoroTimer
     # manually or if no pomodoros
     # left
     def stop?
-      if @stop || @pomodoros < 0
+      if @pomodoros < 0 || @stop
         @pomodoros = @total
         @stop = false
         true
--- spec/slack_pomodoro_timer/timer_spec.rb
+++ spec/slack_pomodoro_timer/timer_spec.rb
@@ -2,30 +2,60 @@ require 'spec_helper'
 require 'slack_pomodoro_timer/timer'
 
 describe SlackPomodoroTimer::Timer do
+
   let(:timer) { SlackPomodoroTimer::Timer.new }
+
 
   before do
     timer.pomodoros = 1
     timer.interval = 1
   end
 
+
   describe '#start' do
+
+    before do
+      allow(timer).to receive(:sleep)
+    end
+
+
     it 'runs a block on timer start' do
       expect { |b| timer.start(&b) }.to yield_successive_args(1, 0)
     end
   end
 
 
-  describe '#stop' do
-    it 'stops the timer from running' do
-      timer.pomodoros = 2
-      Thread.new do
-        timer.start {}
-      end
-      # The call to timer.stop doesn't
-      # affect the outcome of the test
+  describe '#stop?' do
+    
+    before do
+      allow(timer).to receive(:sleep)
+    end
+
+
+    it 'returns false when the timer has pomodoros' do
+      expect(timer.send(:stop?)).to eq(false)
+    end
+
+
+    it 'returns true after timer#stop is called and pomodoros are ignored' do
       timer.stop
-      expect(timer.pomodoros).to eq(2)
+      expect(timer.send(:stop?)).to eq(true)
+    end
+
+
+    it 'returns true when pomodoros are less than 0' do
+      timer.pomodoros = -1
+      expect(timer.send(:stop?)).to eq(true)
+    end
+  end
+
+
+  describe '#stop' do
+    it 'stops the timer from running the next loop' do
+      expect(timer).to receive(:sleep).exactly(1).times
+      timer.pomodoros = 2
+      timer.stop
+      timer.start {}
     end
   end
 
